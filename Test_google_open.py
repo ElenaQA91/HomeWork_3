9fb03da09ff25fa6c65062e3a58fd27f967c558d
Elena <elenakolesnikova141@gmail.com>
2024-09-22 11:52:47 +0500

    добавила def

---
+++ Test_google_open.py
@@ -0,0 +1,7 @@
+
+from selene import browser, be, have
+
+def test_search(browser_size,open_browser)
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))

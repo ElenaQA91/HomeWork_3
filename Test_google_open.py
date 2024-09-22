d0687428aa303892ea97be1ba34f38081b184086
Elena <elenakolesnikova141@gmail.com>
2024-09-22 13:44:51 +0500

    Добавила тест на поиск текста

--- Test_google_open.py
+++ Test_google_open.py
@@ -1,7 +1,13 @@
 
 from selene import browser, be, have
 
-def test_search(browser_size,open_browser)
+def test_open_browser():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+def test_negative_search():
+    text = "*_*"
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type(text).press_enter()
+    browser.element('[id="search"]').should(have.text('Ответы Mail: *_* что значит?'))
